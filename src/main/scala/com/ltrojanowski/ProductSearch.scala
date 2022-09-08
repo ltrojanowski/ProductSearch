b2e72b0a2f57801e952f5aecbf144444622697bd
ltrojanowski <luk.trojanowski@gmail.com>
2022-09-08 21:31:09 +0200

    reimplement deepLeft based on type and not tree of expression passed to macro

--- src/main/scala/com/ltrojanowski/ProductSearch.scala
+++ src/main/scala/com/ltrojanowski/ProductSearch.scala
@@ -3,6 +3,7 @@ package com.ltrojanowski
 import scala.annotation.tailrec
 import scala.language.experimental.macros
 import scala.reflect.macros.whitebox
+import scala.reflect.macros.blackbox
 
 object ProductSearch {
 
@@ -19,15 +20,68 @@ object ProductSearch {
   def deepLeft_impl[A: c.WeakTypeTag](c: whitebox.Context)(a: c.Expr[A]): c.Expr[Any] = {
     import c.universe._
 
+    def typeParams(t: Type): List[Type] = {
+      val tClass = t.typeSymbol.asClass
+      tClass.typeParams.map(_.asType.toType.asSeenFrom(t, tClass))
+    }
+
     @tailrec
-    def findDeepestLeftInNestedTuples(tr: Tree, depth: Int = 0): Tree = {
-      tr.children.tail.headOption match {
-        case Some(t) if (isTupleSymbol(t.tpe.typeSymbol.name.toString)) => findDeepestLeftInNestedTuples(t, depth + 1)
-        case Some(t) => t
+    def findDeepestLeftInNestedTuples(tpe: Type, depth: Int = 0): Int = {
+      typeParams(tpe).headOption match {
+        case Some(t) if isTupleSymbol(t.typeSymbol.name.toString) => {
+          c.info(c.enclosingPosition, s"in is Tuple symbol case: (depth = $depth)", false)
+          findDeepestLeftInNestedTuples(t, depth + 1)
+        }
+        case Some(t) => depth
         case None => sys.error("Tuple with no elements (´⊙ω⊙`)ʷᵗᶠ")
       }
     }
-    val deepElem = findDeepestLeftInNestedTuples(a.tree)
-    c.Expr(deepElem)
+    val depth = findDeepestLeftInNestedTuples(weakTypeOf[A])
+    val result = (0 to depth).foldLeft(a.tree) { case (t, _) => Select(t, TermName("_1")) }
+    c.Expr(result)
+  }
+
+  def find[A <: Product, B](a: A): B = macro find_impl[A, B]
+
+  def find_impl[A: c.WeakTypeTag, B: c.WeakTypeTag](c: blackbox.Context)(a: c.Expr[A]): c.Expr[B] = {
+    import c.universe._
+    val bType: Type = weakTypeOf[B]
+
+    @tailrec
+    def moveToSiblingOrBacktrack(curNode: Tree, prevNodes: List[Tree]): (Tree, List[Tree]) = {
+      prevNodes.headOption match {
+        case None => sys.error(s"This tuple does not contain the searched for type ${bType.typeSymbol.name}")
+        case Some(parent) => {
+          val indexOfCurrent = parent.children.indexOf(curNode)
+          try {
+            (parent.children(indexOfCurrent + 1), prevNodes)
+          } catch {
+            case e: IndexOutOfBoundsException => {
+              val tail = prevNodes.tail
+              val newCurrent = prevNodes.head
+              moveToSiblingOrBacktrack(newCurrent, tail)
+            }
+          }
+        }
+      }
+    }
+
+    @tailrec
+    def findTypeInNestedTuples(currentNode: Tree, previousNodes: List[Tree] = Nil): Tree = {
+      if (currentNode.tpe == bType) {
+        currentNode
+      } else if (isTupleSymbol(currentNode.tpe.typeSymbol.name.toString)) {
+        // check if tuple and if it is go deeper
+        findTypeInNestedTuples(currentNode.children(1), currentNode :: previousNodes)
+      } else {
+        // is not tuple and not searched for value, so in parent check next after current node
+        // if next after current node does not exist then backtrack
+        // if no parent exists then the value does not exist in this tuple
+        val (newCur, newPrev) = moveToSiblingOrBacktrack(currentNode, previousNodes)
+        findTypeInNestedTuples(newCur, newPrev)
+      }
+    }
+
+    c.Expr(findTypeInNestedTuples(a.tree, Nil))
   }
 }
--- src/test/scala/com/ltrojanowski/ProductSearchTest.scala
+++ src/test/scala/com/ltrojanowski/ProductSearchTest.scala
@@ -5,7 +5,17 @@ class ProductSearchTest extends munit.FunSuite {
   case class Foo(f: String)
 
   test("return value of ProductSearch.first") {
-    val actual: Foo = ProductSearch.deepLeft(Tuple3(Tuple2(Tuple2(Foo("asdf"), 1), "asdf"), "asdf", 1.0))
+    val fooTpe = Tuple3(Tuple2(Tuple2(Foo("asdf"), 1), "asdf"), "asdf", 1.0)
+    val actual: Foo = ProductSearch.deepLeft(fooTpe)
+    assert(actual == Foo("asdf"))
+  }
+
+  test("return value of ProductSearch.find[Foo]") {
+    val actual: Foo =
+      ProductSearch
+        .find[(((Foo, Int), String), String, Double), Foo](
+          Tuple3(Tuple2(Tuple2(Foo("asdf"), 1), "asdf"), "asdf", 1.0)
+        )
     assert(actual == Foo("asdf"))
   }
 }
